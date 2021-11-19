b8d2218643ffa56a55100c1b5aab5bf6d3e3c776
iamuRon <93888896+iamuRon@users.noreply.github.com>
2021-11-19 04:10:48 -0500

    IT DIDNT UPDATE SO I DID IT MYSELF

--- RaiseBOT.py
+++ RaiseBOT.py
@@ -9,7 +9,7 @@ https://docs.pycord.dev/en/master/api.html
 
 '''
 import config
-from discord import message
+from discord import channel, message
 from discord.ext import commands, tasks
 import discord
 import json
@@ -17,7 +17,11 @@ import os
 import sqlite3
 from itertools import cycle
 import asyncio
- 
+import json
+import requests
+from discord.ext import tasks
+from twitchAPI.twitch import Twitch
+
 intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix= ".", intents = intents)
@@ -59,6 +63,68 @@ def db_lookup(user):
         db.commit()
         return findUser
 
+#-------------------------------------------------------
+
+#              This holds twitch live notifications I *USED TO NOT* KNOW WAT THE FUCK THIS EVEN DOES I WILL LOOK INTO LATER JUST WANNA KNOW IF IT WORKS
+
+#-------------------------------------------------------
+
+@tasks.loop(seconds=10)
+async def twitchstream():
+    with open('streamers.txt', 'r') as file:
+        streamers = list(file.readlines())
+    for i in streamers:
+        if "\n" in i:
+            StName = i.strip()
+            with open('channelids.txt', 'r') as file:
+                channels = list(file.readlines())
+            for i in channels:
+                if "\n" in i:
+                    channelid = i.strip()
+                    newIDint = int(channelid)
+            await streamer(StName, newIDint)
+
+
+async def streamer(name, channelid):
+    StName = name
+    placeholderList = []
+    channel = bot.get_channel(channelid)
+    client_id = 'iew89f33r9gr771zwbmhen4d2guu7i'
+    client_secret = 'oghcuhwz9c3pzrwe1r8itp3x9qe44g'
+    body = {'client_id': client_id,'client_secret': client_secret,"grant_type": 'client_credentials'}
+    r = requests.post('https://id.twitch.tv/oauth2/token', body)
+    keys = r.json()
+    headers = {'Client-ID': client_id,'Authorization': 'Bearer ' + keys['access_token']}
+    stream = requests.get('https://api.twitch.tv/helix/streams?user_login=' + StName, headers=headers)
+    stream_data = stream.json()
+    if len(stream_data['data']) == 1:
+        placeholderList.append(StName)
+        placeholder = StName + ' is live: ' + stream_data['data'][0]['title'] + ' playing ' + stream_data['data'][0]['game_name']
+        async for history in channel.history(limit=200):
+            if StName not in history.content:
+                for i in placeholderList:
+                    if i != StName:
+                        await channel.send(placeholder)
+                        return
+                else:
+                    return
+    else:
+        if StName in placeholderList:
+            placeholderList.remove(StName)
+            print(placeholderList)
+        else:
+            return
+    
+
+
+
+#-------------------------------------------------------
+
+#              This holds twitch live notifications I *USED TO NOT* KNOW WAT THE FUCK THIS EVEN DOES I WILL LOOK INTO LATER JUST WANNA KNOW IF IT WORKS
+
+#-------------------------------------------------------
+
+
 class MyClient(discord.Client):
     
 
@@ -71,10 +137,13 @@ class MyClient(discord.Client):
         status = ['Use .help for commands','Coded by: iamu & PaliKai', f'A part of {serverCount} servers!']
         displaying = cycle(status)
         running = True
+        await twitchstream.start()
         while running:
             currentStatus = next(displaying)
             await bot.change_presence(status=discord.Status.online, activity=discord.Activity(name=currentStatus ,type=2))
             await asyncio.sleep(20)
+        
+        
 
     @bot.event
     async def on_join(member):
@@ -85,13 +154,21 @@ class MyClient(discord.Client):
     @bot.event
     async def on_message(message):
         context = [f"Server: {message.author.guild} -> Channel: {message.channel} -> User: {message.author} | {message.created_at} : {message.content}"]
-        with open("chatlog.txt", "a") as chatlog:
-            chatlog.write(f"{context}\n")
-        chatlog.close()
+        try:
+            with open("chatlog.txt", "a") as chatlog:
+                chatlog.write(f"{context}\n")
+            chatlog.close()
+        except UnicodeError:
+            pass
         db_lookup(message.author)
         db_balInc(message.author)
-        await bot.process_commands(message)
-
+        try:
+            if message.content[0] != '.':
+             return
+            else:
+                await bot.process_commands(message)
+        except IndexError:
+            return
 
 
 @bot.command()
@@ -108,15 +185,33 @@ async def bal(ctx):
     userBal = strBal
     embed=discord.Embed(title="Balance", color=0xff00f7)
     embed.set_author(name="RaiseBot")
+    embed.set_thumbnail(url="https://i.imgur.com/3QwRkoS.png")
     embed.add_field(name=f"--- {ctx.author}'s Wallet ---", value=f"Your CaveCoin: {userBal}", inline=False)
     embed.set_footer(text="Coded by: iamu")
     await ctx.send(f"Here you go {ctx.author.mention} !")
     await ctx.send(embed=embed)
             
+@bot.command(name='addtwitch', help='Adds your Twitch to the live notifs. (ADMIN ONLY)', pass_context=True)
+async def add_twitch(ctx, twitch_name):
+    if ctx.author.guild_permissions.administrator:
+        # Adds the changes we made to the json file.
+        with open('streamers.txt', 'a') as file:
+            file.writelines(f"{twitch_name}\n")
+        # Tells the user it worked.
+        await ctx.send(f"Added {twitch_name} for {ctx.author} to the notifications list.")
+    else:
+        await ctx.send(f"Sorry bozo you're not the boss of me!")
 
-
-
-        
+@bot.command(name='twitchchannel', aliases=['streamnotif'])
+async def add_twitch_notification_channel(ctx, channelID):
+    """Declares a channel in the server to be the twitch notif channel (ADMIN ONLY)"""
+    if ctx.author.guild_permissions.administrator:
+        with open('channelids.txt', 'a') as file:
+            file.writelines(f"{channelID}\n")
+        # Tells the user it worked.
+        await ctx.send(f"Added {channelID} for {ctx.author} to the notifications list.")
+    else:
+        await ctx.send(f"Sorry bozo you're not the boss of me!")
             
             
             
@@ -140,7 +235,7 @@ Dont Touch Below This Comment as it is starting parameters
 
 client = MyClient()
 
-initial_extensions = ["cogs.commands", "cogs.palisprojects", "cogs.media"]
+initial_extensions = ["cogs.commands", "cogs.palisprojects", "cogs.media", "cogs.leaderboard"]
 
 print("Running Setup...")
 
